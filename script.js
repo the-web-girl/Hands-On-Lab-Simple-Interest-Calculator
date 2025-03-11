8fb3141c2e506a08bbe3ec5d4188e494db7b14bf
the-web-girl <angelique@web-i.org>
2025-03-11 15:01:56 +0100

    adding variables in function compute

--- script.js
+++ script.js
@@ -10,5 +10,12 @@ function updateRate() {
 // Creating a function and variables for the calculation button functionality
 
 function compute() {
-    
+    // implémentation des variables
+    // implementation of variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate / 100;
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
 }
