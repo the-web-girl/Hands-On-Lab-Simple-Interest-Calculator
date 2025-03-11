77db80d3e6f77f1ba2c0cb8fdd9d8327f908a5f7
the-web-girl <angelique@web-i.org>
2025-03-11 14:08:11 +0100

    add function compute

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Création d'une fonction pour modifier la valeur du curseur "taux"
+// Creating a function to change the value of the "rate" slider
+
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
+}
+
+// Création d'une fonction et de variables pour les fonctionnalités du bouton de calcul
+// Creating a function and variables for the calculation button functionality
+
+function compute() {
+    
 }
